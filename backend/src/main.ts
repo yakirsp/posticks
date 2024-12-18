89dc4a45fc1f724d669c8fdf47062fcbfe9518e5
yakirsp <yakirsp@icloud.com>
2024-12-18 22:52:42 +0300

    add trpc backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,10 @@
 import express from 'express';
-
-const posts = [
-    {title: "post 1", text: "post 1 text"},
-    {title: "post 2", text: "post 2 text"},
-    {title: "post 3", text: "post 3 text"},
-    {title: "post 4", text: "post 4 text"},
-    {title: "post 5", text: "post 5 text"},
-    {title: "post 6", text: "post 6 text"},
-    {title: "post 7", text: "post 7 text"},
-]
+import * as trpcExpress from '@trpc/server/adapters/express';
+import { trpcRouter } from './trpc';
 
 const app = express();
-app.use(express.json())
-app.get('/ping', (req, res) => {
-  res.send({result: "ok"});
-});
-app.use('/posts', (req, res) => {
-    res.send(posts);
-})
+app.use(express.json());
+app.use('/trpc', trpcExpress.createExpressMiddleware({ router: trpcRouter }));
 
 app.listen(4321, () => {
   console.info('Listening at http://localhost:4321');
---
+++ backend/src/trpc.ts
@@ -0,0 +1,21 @@
+import { initTRPC } from '@trpc/server';
+
+const trpc = initTRPC.create();
+
+const posts = [
+  { title: 'post 1', text: 'post 1 text' },
+  { title: 'post 2', text: 'post 2 text' },
+  { title: 'post 3', text: 'post 3 text' },
+  { title: 'post 4', text: 'post 4 text' },
+  { title: 'post 5', text: 'post 5 text' },
+  { title: 'post 6', text: 'post 6 text' },
+  { title: 'post 7', text: 'post 7 text' },
+];
+
+export const trpcRouter = trpc.router({
+  getPosts: trpc.procedure.query(() => {
+    return { posts };
+  }),
+});
+
+export type TcpRouter = typeof trpcRouter;
