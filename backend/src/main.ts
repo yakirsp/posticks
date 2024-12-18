e8dd2bfcd967d3e49427368b916b2a98225291e3
yakirsp <yakirsp@icloud.com>
2024-12-18 22:38:37 +0300

    create test endpoints

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,24 @@
-console.log('Hello World');
+import express from 'express';
+
+const posts = [
+    {title: "post 1", text: "post 1 text"},
+    {title: "post 2", text: "post 2 text"},
+    {title: "post 3", text: "post 3 text"},
+    {title: "post 4", text: "post 4 text"},
+    {title: "post 5", text: "post 5 text"},
+    {title: "post 6", text: "post 6 text"},
+    {title: "post 7", text: "post 7 text"},
+]
+
+const app = express();
+app.use(express.json())
+app.get('/ping', (req, res) => {
+  res.send({result: "ok"});
+});
+app.use('/posts', (req, res) => {
+    res.send(posts);
+})
+
+app.listen(4321, () => {
+  console.info('Listening at http://localhost:4321');
+});
